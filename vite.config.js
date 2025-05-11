530914a73a3f77a1f34a543c8a7cc0f5424b5ef2
artyom <plokhikhartyom10@gmail.com>
2025-05-11 22:58:17 +0200

    internet indicator fix; pwa cache fix

--- vite.config.js
+++ vite.config.js
@@ -35,7 +35,7 @@ export default defineConfig(({ mode }) => {
                     ]
                 },
                 workbox: {
-                    globPatterns: ['**/*.{js,css,html,ico,png,svg,mp4}'],
+                    globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
                     runtimeCaching: [{
                         urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
                         handler: 'CacheFirst',
